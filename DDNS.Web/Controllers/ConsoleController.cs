21f4e26bac08803690c2c2f8ac1063355eea8f32
阿星Plus <hackxing@foxmail.com>
2018-12-21 16:22:00 +0800

    语言资源文件、公共_Layout页面

---
+++ DDNS.Web/Controllers/ConsoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DDNS.Web.Controllers
+{
+    public class ConsoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
