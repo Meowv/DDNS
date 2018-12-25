77653ba5e417b3d600388f97811e4e80f7676bd2
阿星Plus <hackxing@foxmail.com>
2018-12-25 22:57:44 +0800

    隧道管理实体类

---
+++ DDNS.Entity/Tunnel/TunnelEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DDNS.Entity.Tunnel
+{
+    public class TunnelEntity
+    {
+        public int TunneId { get; set; }
+
+        public int UserId { get; set; }
+
+        public string TunnelProtocol { get; set; }
+
+        public string TunnelName { get; set; }
+
+        public string SubDomain { get; set; }
+
+        public string LocalPort { get; set; }
+
+        public int Status { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime OpenTime { get; set; }
+
+        public DateTime ExpiredTime { get; set; }
+
+        public string FullUrl { get; set; }
+    }
+}
---
+++ DDNS.Entity/Tunnel/TunnelStatusEnum.cs
@@ -0,0 +1,20 @@
+﻿namespace DDNS.Entity.Tunnel
+{
+    public enum TunnelStatusEnum
+    {
+        /// <summary>
+        /// 待审
+        /// </summary>
+        Audit = 0,
+
+        /// <summary>
+        /// 通过
+        /// </summary>
+        Pass = 1,
+
+        /// <summary>
+        /// 拒绝
+        /// </summary>
+        Refuse = -1
+    }
+}
